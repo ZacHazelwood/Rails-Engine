8a9da0e0281622b03476872c325de498eb08e582
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-12 13:56:44 -0600

    Feat: GET all Merchants request

--- spec/requests/api/v1/merchants_requests_spec.rb
+++ spec/requests/api/v1/merchants_requests_spec.rb
@@ -14,15 +14,16 @@ RSpec.describe 'Merchants API requests' do
     expect(merchants.count).to be 5
 
     merchants.each do |merchant|
-      expect(merchant).to be_a Array
+      expect(merchant).to be_a Hash
+      expect(merchant).to have_key (:id)
       expect(merchant[:id]).to be_a String
 
       expect(merchant).to have_key(:type)
       expect(merchant[:type]).to be_a String
-      expect(merchant[:type]).to be("merchant")
+      expect(merchant[:type]).to eq("merchant")
 
       expect(merchant[:attributes]).to have_key(:name)
-      expect(merchant[:attributes[:name]]).to be_a String
+      expect(merchant[:attributes][:name]).to be_a String
     end
   end
 end
