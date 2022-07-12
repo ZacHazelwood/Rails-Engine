cd94c134180e19c69f468a6870e8ffd5a5159013
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-12 13:43:19 -0600

    Test: GET all Merchants request

---
+++ spec/requests/api/v1/merchants_requests_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchants API requests' do
+  it 'sends a list of all merchants' do
+    create_list(:merchant, 5)
+
+    get '/api/v1/merchants'
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    merchants = response_body[:data]
+
+    expect(response).to be_successful
+    expect(merchants).to be_a Array
+    expect(merchants.count).to be 5
+
+    merchants.each do |merchant|
+      expect(merchant).to be_a Array
+      expect(merchant[:id]).to be_a String
+
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to be_a String
+      expect(merchant[:type]).to be("merchant")
+
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes[:name]]).to be_a String
+    end
+  end
+end
