c454631c295ca7b0e9aed063f6824679bd98b0c4
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-12 14:41:56 -0600

    Test: GET Merchant Items request or error

--- spec/requests/api/v1/merchants_requests_spec.rb
+++ spec/requests/api/v1/merchants_requests_spec.rb
@@ -55,4 +55,54 @@ RSpec.describe 'Merchants API requests' do
 
     expect(response.status).to eq(404)
   end
+
+  it "sends a list of items belonging to a merchant" do
+    merchant = create(:merchant)
+    item_1 = create(:item, merchant_id: merchant.id)
+    item_2 = create(:item, merchant_id: merchant.id)
+    item_3 = create(:item, merchant_id: merchant.id)
+
+    get "/api/v1/merchants/#{merchant.id}/items"
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    items = response_body[:data]
+
+    expect(response).to be_successful
+    expect(items).to be_a Array
+    expect(items.count).to eq 3
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_a String
+
+      expect(item).to have_key(:type)
+      expect(item[:type]).to be_a String
+      expect(item[:type]).to eq("item")
+
+      expect(item).to have_key(:attributes)
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a String
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a String
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a Float
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a Integer
+    end
+  end
+
+  it "sends an error if a merchant is not found, items" do
+    merchant = create(:merchant, id: 5)
+    item_1 = create(:item, merchant_id: merchant.id)
+    item_2 = create(:item, merchant_id: merchant.id)
+    item_3 = create(:item, merchant_id: merchant.id)
+
+    get "/api/v1/merchants/6/items"
+
+    expect(response.status).to eq(404)
+  end
 end
